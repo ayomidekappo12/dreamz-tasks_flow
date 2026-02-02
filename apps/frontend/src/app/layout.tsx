c44c36883aec2fad13d8c98d4bca06c6a132ef7d
ayomidekappo12 <ayomidekappo12@gmail.com>
2026-02-02 08:41:36 -0600

    cleared viewport error

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import App from "@/components/layout/app";
 import { Sora, Inter } from "next/font/google";
@@ -88,12 +88,16 @@ const organizationSchema = {
   },
 };
 
+export const viewport: Viewport = {
+  themeColor: "#ffffff",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html suppressHydrationWarning lang="en" className={fontClass}>
       <head>
