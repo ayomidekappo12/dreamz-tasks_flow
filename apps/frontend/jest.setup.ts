0b47e30a14b1c3b4f5b51c127be5b2179f31c631
ayomidekappo12 <ayomidekappo12@gmail.com>
2026-02-02 07:27:23 -0600

    deployment cleaning up

--- apps/frontend/jest.setup.ts
+++ apps/frontend/jest.setup.ts
@@ -1,13 +1,4 @@
 // jest.setup.ts
 
-// Extend Jest with custom DOM matchers from React Testing Library
-import "@testing-library/jest-dom";
-import "fake-indexeddb/auto"; // gives Jest a working indexedDB
-
-// Optional: mock Next.js router if needed
-import { loadEnvConfig } from "@next/env";
-
-loadEnvConfig(process.cwd());
-
-// Example: mock next/router
-jest.mock("next/router", () => require("next-router-mock"));
+// If you only need envs in tests, Next already loads them
+// or you can rely on process.env directly
